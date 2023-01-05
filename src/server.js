7f81822cff2c2db111ee2fa4e65e3c9e1fc3bbdb
Deyvid Kasteh <kasteh21@gmail.com>
2023-01-05 01:23:06 -0300

    add index js root file

--- src/server.js
+++ src/server.js
@@ -1,3 +1,10 @@
 import app from "./app.js"
+
+export function server() {
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>console.log("Connect"));
+app.listen(PORT, () => console.log("Connect"));
+
+}
+
+
